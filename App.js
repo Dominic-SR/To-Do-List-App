1547204e578340ad41bb4091f8fe6a968a52ee71
dominic <dominicseduraja3@gmail.com>
2024-08-29 23:44:34 +0530

    tailwind init

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  
   return (
-  <View className="flex-1 items-center justify-center bg-black">
-      <Text>Open up App.js to start working on your app!</Text>
+    // <NavigationContainer>
+      // <Stack.Navigator>
+      <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-red-500">Do To List!</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+      // </Stack.Navigator>
+    //  </NavigationContainer> 
   );
 }
